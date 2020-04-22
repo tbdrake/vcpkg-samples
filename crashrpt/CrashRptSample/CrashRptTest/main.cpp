76c5566c8f1e1bb0469613710d20e5cb3d7e8b34
Travis Drake <tdrake@qsrautomations.com>
2020-04-22 12:03:48 -0400

    Rename configurations that uses static libraries and add configurations for x86-windows and x64-windows triplets to test using dlls.

---
+++ crashrpt/CrashRptSample/CrashRptTest/main.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include <gtest/gtest.h>
+
+// main() added because tests are not discovered for some reason with gtest_main.dll
+// https://github.com/google/googletest/issues/2157
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
